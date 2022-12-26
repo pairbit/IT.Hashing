65de7f01a75f2804580a700f9cdb9ef78193f2b2
Ivan Tikhonov <itikhonov@mail.com>
2022-12-26 11:26:16 +0300

    init

---
+++ IT.Hashing.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace IT.Hashing.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+
+        Assert.Pass();
+    }
+}
