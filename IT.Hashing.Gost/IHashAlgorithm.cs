f01c7a00ccd382ef81966add84edaacc77a6c1c3
Ivan Tikhonov <pairbit@gmail.com>
2026-08-11 11:38:55 +0300

    fix

--- IT.Hashing.Gost/IHashAlgorithm.cs
+++ IT.Hashing.Gost/IHashAlgorithm.cs
@@ -14,7 +14,6 @@ public interface IHashAlgorithm : IDisposable
 
     void Append(byte[] array, int start, int length);
 
-    //TryGetHashAndReset
     bool TryGetHash(Span<byte> hash, out int length);
 
     bool TryGetHashInBase64(Span<byte> hash, out int length);
